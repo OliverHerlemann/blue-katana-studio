213e6486aff11787cd202eba1cf3f5ae476cc0f4
oliverHerlemann <oliver.herlemann@t-online.de>
2026-07-14 12:57:28 +0200

    added the LangButton and implemented the functions regarding the lang switch

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
+"use client"
+
 import styles from "./page.module.css";
 import Header from "../components/Header.jsx";
 import Navigation from "../components/Navigation.jsx";
+import { useLanguage } from "@/context/LanguageContext";
+import LangButton from "@/UI/Buttons/LangButton.jsx";
 
 export default function Home() {
+  const { translations, setLang } = useLanguage();
+  const t = translations.home;
+
+
   return (
     <div className={styles.page}>
-      <Header/>
+      <Header t={t} />
+      <LangButton t={t} setLang={setLang}/>
       <Navigation />
       <main>
         
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import styles from "./Header.module.css";
 
-export default function Header() {
+export default function Header({t}) {
     return <header className={styles.header}>
         <div className={styles.mainHeader}>
             <h1>Blue <span>Katana</span> Studio</h1>
             <img src="/blue_katana_horizontal.png" alt="A blue Katana" />
-            <h2>Webdesign and Development</h2>
+            <h2>{t.Heading2}</h2>
         </div>
     </header>
 }
