5d65aa5d6edc059874c465958d3dcd345c31e5c2
oliverHerlemann <oliver.herlemann@t-online.de>
2026-07-16 12:09:47 +0200

    improved hovere effect of the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,11 @@ import styles from "./Header.module.css";
 
 export default function Header({t, openContactModal}) {
     return <header className={styles.header}>
-        <div className={styles.mainHeader} onClick={openContactModal} >
+        <div className={styles.mainHeader} >
             <h1>Blue <span>Katana</span> Studio</h1>
             <img src="/blue_katana_horizontal.png" alt="A blue Katana" />
             <h2>{t.Heading2}</h2>
+            <div className={styles.clickOverlay} onClick={openContactModal} />
         </div>
     </header>
 }
